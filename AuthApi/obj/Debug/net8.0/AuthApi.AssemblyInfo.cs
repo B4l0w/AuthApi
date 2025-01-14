30e23079259474ea6a3d19330153f54e173ea5fd
csanalosibalint <csanalosib@kkszki.hu>
2025-01-14 11:20:12 +0100

    Packages are ready to use.

--- AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
+++ AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa6a3509fb8b6c5fde71ffca5ee784e7cf37c4a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
